2996ee76d9f62e33acd8c56818e0757be517a6f4
Michael McKenna <mdmckenna12@gmail.com>
2020-08-11 16:12:45 -0500

    Initial Commit

---
+++ google-maps-swiftpm-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  google-maps-swiftpm-test
+//
+//  Created by Michael McKenna on 8/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
